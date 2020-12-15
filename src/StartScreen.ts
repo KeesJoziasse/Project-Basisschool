0f11caba0f442aa99b4a184d134b984bea42a9b4
ThomasMelis <Meli0033@hz.nl>
2020-12-15 13:42:01 +0100

    update begin

---
+++ src/StartScreen.ts
@@ -0,0 +1,11 @@
+class Start{
+    private canvas: HTMLCanvasElement;
+    private imgStart: HTMLImageElement;
+    private title: string;
+
+    public constructor(canvas: HTMLCanvasElement){
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+
+    }
+}
