756d252ed1da03698b83a0c3efb9eabcb674bef4
RobvandeVijver <vijv0011@hz.nl>
2021-01-04 14:52:43 +0100

    new start

--- src/App.ts
+++ src/App.ts
@@ -2,7 +2,7 @@ console.log("The game is working");
 
 // Initialize the game after the DOM is loaded.
 let init = () => {
-    new Game (document.getElementById("canvas") as HTMLCanvasElement);
+    new Start (document.getElementById("canvas") as HTMLCanvasElement);
 };
 
 // Add EventListener to load the game whenever the browser is ready
