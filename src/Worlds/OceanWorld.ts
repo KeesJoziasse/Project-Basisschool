e69cd705bee6c7d74d9fb183fd7bb6ba31801058
RobvandeVijver <vijv0011@hz.nl>
2021-01-05 23:54:04 +0100

    BG FIXED

--- src/Worlds/OceanWorld.ts
+++ src/Worlds/OceanWorld.ts
@@ -1,8 +1,20 @@
 /// <reference path="../Game.ts" />
 
 class OceanWorld extends Game {
+
+  private image:HTMLImageElement;
+
   constructor(canvas: HTMLCanvasElement, worldName: string) {
     super(canvas, worldName);
+    this.image = GameItem.loadNewImage("./assets/img/world/OceanBG.jpg");
+    const ctx = this.canvas.getContext("2d");
+  }
+
+  public drawBackground(ctx:CanvasRenderingContext2D){
+    ctx.drawImage(this.image,
+    this.canvas.width / 2,
+    this.canvas.height / 2,
+    )
   }
 
   public frameIndex() {
