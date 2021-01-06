adb2d42d7c05c0cc81b4f350abec420189e56079
JoziasseK <jozi0018@hz.nl>
2021-01-06 11:33:02 +0100

    Changed arctic planet

---
+++ src/Images/ArcticPlanet.ts
@@ -0,0 +1,12 @@
+/// <reference path = "Images.ts"/>
+
+class ArcticPlanet extends Images {
+
+  public constructor(xPos: number, yPos: number) {
+      super(xPos, yPos);
+      this.name = "ArcticPlanet";
+      this.image = Start.loadNewImage(
+        "./assets/img/world/ArcticPlanet.png"
+      );
+    }
+}
